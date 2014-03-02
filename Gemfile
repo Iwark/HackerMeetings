e13c4bf2c6a424952d9b4ade0dd22f13ee5b7390
Iwark <iwark02@gmail.com>
2014-03-02 14:51:06 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.3'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+
+gem 'jbuilder', '~> 1.2'
+
+gem 'turbolinks'
+
+gem 'devise'
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'jazz_hands'
+end
+
+group :development do
+  gem 'better_errors'
+  gem 'annotate'
+end
+
+group :production do
+  gem 'pg'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerMeetings::Application.config.secret_key_base = 'f4f28c85c0def1d5f5ec5302b0e7f79ea07d9f7847032d39790fa7b65adc8e73a04ab8c52f56833b6e549ae427d73584aa067bc2dc0f90b3385461b82bb1e32a'
